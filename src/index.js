567331b1f4f7ee288229c4ecdb7022c0b6e9e9c3
Chasity Wrather <cd1205@gmail.com>
2023-11-23 18:10:08 -0600

    Router Setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 import 'font-awesome/css/font-awesome.css';
@@ -16,9 +17,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </Provider>
   </React.StrictMode>
 );
 
